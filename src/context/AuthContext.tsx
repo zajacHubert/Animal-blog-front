79b57bb827654ac9b942b980f61b54e3f15fa76f
Hubert <hubert.zajac92@gmail.com>
2022-11-03 08:29:44 +0100

    Add auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { createContext, ReactNode, useState } from 'react';
+import { CurrentUser, UserToLogin } from '../types/user';
+
+export const AuthContext = createContext<AuthContextInterface | null>(null);
+
+interface Props {
+    children: ReactNode;
+}
+
+interface AuthContextInterface {
+    currentUser: CurrentUser | null;
+    login: (userToLogin: UserToLogin) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+export const AuthContextProvider = ({ children }: Props) => {
+    const [userFromStorage, setUserFromStorage] = useState<string | null>(localStorage.getItem('user'));
+    const [currentUser, setCurrentUser] = useState<CurrentUser | null>(JSON.parse(userFromStorage!) || null);
+
+
+    const login = async (userToLogin: UserToLogin) => {
+        const res = await axios.post('http://localhost:3001/auth/login', userToLogin);
+        setCurrentUser(res.data);
+    }
+
+    const logout = async () => {
+        await axios.post('http://localhost:3001/auth/logout');
+        setCurrentUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
