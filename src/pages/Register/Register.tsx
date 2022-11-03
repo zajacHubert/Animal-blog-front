d5dfaee34d20e508d7041adde4db7c44802b7ff4
Hubert <hubert.zajac92@gmail.com>
2022-11-03 07:34:40 +0100

    Create register component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserToRegister } from '../../types/user';
 
 const Register = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<UserToRegister>();
+    const navigate = useNavigate();
+    const onSubmit: SubmitHandler<UserToRegister> = async (data) => {
+        try {
+            await axios.post('http://localhost:3001/auth/register', data);
+            navigate('/login');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <div>Register</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" {...register('username', { required: true })} />
+            <input type="text" {...register('email', { required: true })} />
+            <input type="text" {...register('password', { required: true })} />
+            <button>Register</button> <span>Do you have an acount? <Link to='/login'>Login</Link></span>
+        </form>
     )
 }
 
