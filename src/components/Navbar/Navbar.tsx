5c37acc0f9f264e78013e78ecdead2c5cd521e48
Hubert <hubert.zajac92@gmail.com>
2022-11-04 08:17:23 +0100

    Add styles to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,34 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext'
+import { AuthContext } from '../../context/AuthContext';
+import Logo from '../../img/logo.png';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
     const ctx = useContext(AuthContext);
-    console.log(ctx)
     return (
-        <div>
-            <div>
-                <img src="" alt="" />
-            </div>
+        <div className={styles.navbar}>
+            <Link to='/'>
+                <div className={styles.logo}>
+                    <img src={Logo} alt="" />
+                </div>
+            </Link>
             <nav>
-                <ul>
-                    <li>Cats</li>
-                    <li>Dogs</li>
-                    <li>Other</li>
+                <ul className={styles.links}>
+                    <li><Link className={styles.link} to='/?cat=cats'>Cats</Link></li>
+                    <li><Link className={styles.link} to='/cat=dogs'>Dogs</Link></li>
+                    <li><Link className={styles.link} to='/cat=other'>Other</Link></li>
                 </ul>
             </nav>
             {ctx?.currentUser
                 ?
-                <div>
+                <div className={styles.user}>
                     <span>{ctx.currentUser.username}</span>
-                    <span onClick={ctx.logout}>Logout</span>
-                    <span ><Link to='/write'>Write</Link></span>
+                    <span className={styles.log} onClick={ctx.logout}>Logout</span>
+                    <span className={styles.write}><Link to='/write'>Write</Link></span>
                 </div>
                 :
-                <Link to='/login'>Login</Link>
+                <Link className={styles.log} to='/login'>Login</Link>
             }
         </div>
     )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import moment from 'moment';
+import { Post } from '../../types/post'
 
 const Home = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const cat = useLocation().search;
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(`http://localhost:3001/post${cat}`);
+            setPosts(res.data)
+        })()
+    }, [cat]);
     return (
-        <div>Home</div>
+        <div>
+            {posts.map((post: Post) => (
+                <div key={post.id}>
+                    <div>
+                        <Link to={`/single/${post.id}`}>
+                            <h2>{post.title}</h2>
+                        </Link>
+                        <p>{post.desc}</p>
+                        <p>Posted {moment(post.date).fromNow()}</p>
+                    </div>
+                    <div>
+                        <img src={post.img} alt="" />
+                    </div>
+                </div>
+            ))}
+        </div>
     )
 }
 
